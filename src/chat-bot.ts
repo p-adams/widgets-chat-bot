9fd7b0e90dbee0920a9fcca7b63c4c588a08de74
Paul Adams <ghusiyy@gmail.com>
2023-08-08 02:21:45 -0700

    init chat window

--- src/chat-bot.ts
+++ src/chat-bot.ts
@@ -1,16 +1,24 @@
 export function setupChatBot(element: HTMLDivElement) {
   let isOpen = false;
 
-  element.innerHTML = `<div class="chat-bot-inner closed">
-        bot
+  element.innerHTML = `<div class="chat-bot-inner">
+        <div class="chat-bot-window closed">
+          a
+          b
+          c
+        </div>
+        <div class="chat-bot-cog">
+          bot
+        </div>
     </div>`;
 
+  const chatBotWin =
+    document.querySelector<HTMLDivElement>(".chat-bot-window")!;
   const chatBot = document.querySelector<HTMLDivElement>(".chat-bot-inner")!;
-
   function toggleChatBot() {
     isOpen = !isOpen;
-    chatBot.classList.add(isOpen ? "open" : "closed");
-    chatBot.classList.remove(isOpen ? "closed" : "open");
+    chatBotWin.classList.add(isOpen ? "open" : "closed");
+    chatBotWin.classList.remove(isOpen ? "closed" : "open");
   }
 
   chatBot?.addEventListener("click", toggleChatBot);
