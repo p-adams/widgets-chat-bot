726e047c26ff694fb6356e7fb400c7b7b24a7aea
Paul Adams <ghusiyy@gmail.com>
2023-08-08 19:48:48 -0700

    add chat window input and button

--- src/chat-bot.ts
+++ src/chat-bot.ts
@@ -1,11 +1,21 @@
-export function setupChatBot(element: HTMLDivElement) {
-  let isOpen = false;
+function getEl<T extends HTMLElement = HTMLElement>(
+  selector: string
+): T | null {
+  return document.querySelector<T>(selector)!;
+}
 
+export function setupChatBot(element: HTMLDivElement) {
   element.innerHTML = `<div class="chat-bot-inner">
         <div class="chat-bot-window closed">
+          <div class="window">
           a
           b
           c
+          </div>
+         <div class="input-outer">
+          <input placeholder="type message"/>
+          <button>send</button>
+         </div>
         </div>
         <div class="chat-bot-cog-wrapper">
           <div class="chat-bot-cog-inner">
@@ -14,14 +24,27 @@ export function setupChatBot(element: HTMLDivElement) {
         </div>
     </div>`;
 
-  const chatBotWin =
-    document.querySelector<HTMLDivElement>(".chat-bot-window")!;
-  const chatBot = document.querySelector<HTMLDivElement>(".chat-bot-inner")!;
+  const chatBotWin = getEl<HTMLDivElement>(".chat-bot-window");
+  const chatBot = getEl<HTMLDivElement>(".chat-bot-cog-wrapper");
+  const sendMsgBtn = getEl<HTMLButtonElement>(".input-outer > button");
+  const msgInput = getEl<HTMLButtonElement>(".input-outer > input");
+
+  let isOpen = false;
+  let message = "";
+
   function toggleChatBot() {
     isOpen = !isOpen;
-    chatBotWin.classList.add(isOpen ? "open" : "closed");
-    chatBotWin.classList.remove(isOpen ? "closed" : "open");
+    chatBotWin?.classList.add(isOpen ? "open" : "closed");
+    chatBotWin?.classList.remove(isOpen ? "closed" : "open");
   }
 
   chatBot?.addEventListener("click", toggleChatBot);
+  sendMsgBtn?.addEventListener("click", () => {
+    // TODO: handle send message
+    console.log("send", message);
+  });
+  msgInput?.addEventListener("input", (e) => {
+    const target = e.target as HTMLInputElement;
+    message = target?.value;
+  });
 }
